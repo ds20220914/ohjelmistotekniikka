833eab8416416eff8d5b43653130e523ce36f953
shao <junhao.shao@helsinki.fi>
2023-03-22 20:57:42 +0200

    uusi

--- README.md
+++ README.md
@@ -8,7 +8,10 @@
 
 [komentorivi.txt](https://github.com/ds20220914/ohjelmistotekniikka/blob/main/laskarit/viikko1/komentorivi.txt)
 
-**olen helsingin yliopiston opiskelija**
-_tietojenkäsittelytiede_
+### viikko 2
 
-.
+[testikattavuus_raport](https://github.com/ds20220914/ohjelmistotekniikka/blob/main/laskarit/viikko2/testikattavuus_kuva.png)
+[maksukortti_test](https://github.com/ds20220914/ohjelmistotekniikka/tree/main/laskarit/viikko2/maksukortti)
+[unicafe_test](https://github.com/ds20220914/ohjelmistotekniikka/tree/main/laskarit/viikko2/unicafe)
+
+
--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -2,26 +2,46 @@ import unittest
 from kassapaate import Kassapaate
 from maksukortti import Maksukortti
 class TestKassapaate(unittest.TestCase):
-	def test_kassassa_oleva_raha_oikea(self):
-		luku=Kassapaate()
-		self.assertEqual(luku.kassassa_rahaa, 100000)
+	def setUp(self):
+		self.kassapaate=Kassapaate()
 	def test_lounaita_myyty_0(self):
-                luku=Kassapaate()
-                self.assertEqual(luku.edulliset+luku.maukkaat, 0)
-	def test_kateisosto_rahat_riittaa1(self):
-		luku=Kassapaate()
-		maara=luku.syo_edullisesti_kateisella(250)
 		
-		self.assertEqual(luku.edulliset, 1)
-		self.assertEqual(luku.kassassa_rahaa, 100240)
+		self.assertEqual(self.kassapaate.edulliset, 0)
+		self.assertEqual(self.kassapaate.maukkaat, 0)
+		self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+	
+	def test_kateisosto_rahat_riittaa1(self):
+		
+		maara=self.kassapaate.syo_edullisesti_kateisella(250)
+		
+		self.assertEqual(self.kassapaate.edulliset, 1)
+		self.assertEqual(self.kassapaate.maukkaat, 0)
+		self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
 		self.assertEqual(maara, 10)
 	def test_kateisosto_rahat_riittaa2(self):
 		luku=Kassapaate()
                 
 		maara1=luku.syo_maukkaasti_kateisella(410)
-		self.assertEqual(luku.edulliset+luku.maukkaat, 1)
+		self.assertEqual(luku.maukkaat, 1)
+		self.assertEqual(luku.edulliset, 0)
 		self.assertEqual(luku.kassassa_rahaa, 100400)
 		self.assertEqual(maara1, 10)
+	
+	def test_kateisosto_rahat_riittaa3(self):
+		luku=Kassapaate()
+                
+		maara1=luku.syo_maukkaasti_kateisella(400)
+		self.assertEqual(luku.maukkaat, 1)
+		self.assertEqual(luku.kassassa_rahaa, 100400)
+		self.assertEqual(maara1, 0)
+
+	def test_kateisosto_rahat_riittaa4(self):
+		luku=Kassapaate()
+                
+		maara1=luku.syo_edullisesti_kateisella(240)
+		self.assertEqual(luku.edulliset, 1)
+		self.assertEqual(luku.kassassa_rahaa, 100240)
+		self.assertEqual(maara1, 0)
 
 	def test_kateisosto_rahat_eivat_riittaa(self):
 		luku=Kassapaate()
@@ -99,17 +119,26 @@ class TestKassapaate(unittest.TestCase):
                       
 		self.assertEqual(maara1, False)
 		self.assertEqual(kortti.saldo, 390)
-	def test_lataa_rahaa_kortille(self):
-		luku=Kassapaate()
-		kortti=Maksukortti(0)
-		luku.lataa_rahaa_kortille(kortti,50)
-		self.assertEqual(luku.kassassa_rahaa, 100050)
-		self.assertEqual(kortti.saldo, 50)
 
-	def test_lataa_rahaa_kortille_ei_onnistu(self):
-		luku=Kassapaate()
+	
+	def test_lataa_rahaa(self):
 		kortti=Maksukortti(0)
-		maara=luku.lataa_rahaa_kortille(kortti,-1)
+		luku=Kassapaate()
+		luku.lataa_rahaa_kortille(kortti, 50)
+		self.assertEqual(kortti.saldo, 50)
+		self.assertEqual(luku.kassassa_rahaa, 100050)
+	
+	def test_lataa_rahaa1(self):
+		kortti=Maksukortti(0)
+		luku=Kassapaate()
+		luku.lataa_rahaa_kortille(kortti, 0)
+		self.assertEqual(kortti.saldo, 0)
+		self.assertEqual(luku.kassassa_rahaa, 100000)
+
+	def test_lataa_rahaa2(self):
+		kortti=Maksukortti(0)
+		luku=Kassapaate()
+		maara=luku.lataa_rahaa_kortille(kortti, -1)
+		self.assertEqual(kortti.saldo, 0)
 		self.assertEqual(luku.kassassa_rahaa, 100000)
 		self.assertEqual(maara, None)
-		self.assertEqual(kortti.saldo, 0)
