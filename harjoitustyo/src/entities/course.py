cda8a069e4bbd5daa384b91530923d2f41ec4ee7
ds20220914 <davidkuul2002@gmail.com>
2023-03-28 22:02:31 +0300

    koodit

---
+++ harjoitustyo/src/entities/course.py
@@ -0,0 +1,6 @@
+class Course:
+
+	def __init__(self, course_name,credits,grade=None):
+		self.course_name=course_name
+		self.credits=credits
+		self.grade=grade 
---
+++ harjoitustyo/src/entities/user.py
@@ -0,0 +1,11 @@
+class User:
+
+
+	def __init__(self,username,password,number):
+		self.username=username
+		self.password=password
+		self.role_number=number
+		if self.role_number[0]=="A":
+			self.role=student
+		if self.role_number[0]=="B":
+			self.role=teacher
