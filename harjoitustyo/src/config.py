1b93c39d40320570dac950b8476f1ab875840c0a
ds20220914 <davidkuul2002@gmail.com>
2023-03-29 20:39:19 +0300

    uusi

--- harjoitustyo/src/config.py
+++ harjoitustyo/src/config.py
@@ -2,5 +2,5 @@ import os
 
 dirname=os.path.dirname(__file__)
 
-Smonitor_File_path1=os.path.join(dirname,"..", "data", "Smonitor_username")
+Smonitor_File_path1=os.path.join(dirname,"..", "data", "Smonitor_username.csv")
 
--- harjoitustyo/src/repository/user_repository.py
+++ harjoitustyo/src/repository/user_repository.py
@@ -10,8 +10,10 @@ class UserRepository:
 
 
 	def create(self,user): 
-		db=get_database_connection()
-		db.execute("INSERT INTO User (username,password,role_number) values (?,?,?)",(user.username,user.password,user.role_number))
+		db=self.get.cursor()
+		db.execute("INSERT INTO User (User_name,password,role_number) values (?,?,?)",(user.username,user.password,user.role_number))
+		self.get.commit()
 	def find_all(self):
+		db=self.get.cursor()
 		lista=self.get.execute("SELECT * FROM User ").fetchall()
 		return list(lista))
