4b83b9f50051a048ddb19721f22144aa0ee1c5a7
ds20220914 <davidkuul2002@gmail.com>
2023-04-18 22:56:38 +0300

    ei mtnn

--- harjoitustyo/src/ui/teacher_view1.py
+++ harjoitustyo/src/ui/teacher_view1.py
@@ -29,9 +29,24 @@ class TeacherView1:
         if self.oikea == True:
             array = Services()
             array1 = array.find_by_rolenumber(self.number)
+            course_label4 = ttk.Label(master=self._frame, text="course_name")
+            course_label5 = ttk.Label(master=self._frame, text="grade")
+            course_label6 = ttk.Label(master=self._frame, text="credit")
+            course_label4.grid(row=1, column=0)
+            course_label5.grid(row=1, column=1)
+            course_label6.grid(row=1, column=2)
             for i in range(len(array1)):
-                course_label = ttk.Label(master=self._frame, text=array1[i])
-                course_label.grid(row=i+1, column=0,
-                                  sticky=(constants.E, constants.W))
+
+                course_label1 = ttk.Label(
+                    master=self._frame, text=array1[i]["Course_name"])
+                course_label2 = ttk.Label(
+                    master=self._frame, text=array1[i]["grade"])
+                course_label3 = ttk.Label(
+                    master=self._frame, text=array1[i]["Credit"])
+                course_label1.grid(row=i+2, column=0)
+
+                course_label2.grid(row=i+2, column=1)
+
+                course_label3.grid(row=i+2, column=2)
 
         self._frame.grid_columnconfigure(1, weight=1)
