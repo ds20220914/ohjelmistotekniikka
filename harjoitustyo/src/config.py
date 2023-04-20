223f076ac3b23355ff56359b0cbe25ad2495f79a
ds20220914 <davidkuul2002@gmail.com>
2023-04-20 16:09:34 +0300

    env

--- harjoitustyo/src/config.py
+++ harjoitustyo/src/config.py
@@ -1,12 +1,16 @@
 import os
-
+from dotenv import load_dotenv
 
 dirname = os.path.dirname(__file__)
 
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
 
 DATABASE_FILENAME = os.getenv(
     "Smonitoring_database") or "Smonitoringdatabase.sqlite"
 DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
 
-course_list_FILENAME = os.getenv("Course_database") or "Course_database.sqlite"
-DATABASE_FILE_PATH1 = os.path.join(dirname, "..", "data", course_list_FILENAME)
+COURSE_LIST_FILENAME = os.getenv("Course_database") or "Course_database.sqlite"
+DATABASE_FILE_PATH1 = os.path.join(dirname, "..", "data", COURSE_LIST_FILENAME)
